5505e6ab62589ff6fec7b1659e7aeb7796863ba8
takko-kai <116253178+takko-kai@users.noreply.github.com>
2022-11-16 20:12:04 +0900

    Update 3-2.php

--- 3-2/3-2.php
+++ 3-2/3-2.php
@@ -1,9 +1,25 @@
 <?php
 //step1:フルーツと単価の連想配列を作成
-$fruits = ["りんご" =>150 , "みかん" =>75 , "もも" =>1500 ];
-foreach ($fruits as $key=>$value) {
-    $value=$value*2;
-    echo $key."は".$value."円です。";
+$fruits = ["りんご" =>150 , "みかん" =>30 , "もも" =>500 ];
+
+//配列の0:リンゴ、1:みかん、2:桃の順に個数を配列で作成
+$fruitsv = [2, 5, 6];
+
+function goukei($tanka, $kosu){
+    $ans = $tanka * $kosu;
+    return $ans;
+   }
+   
+   //繰り返し構文
+   $i = 0;
+   foreach ($fruits as $key => $value) {
+       if($i>=3){break;}
+   
+    $price = goukei($value, $fruitsv[$i]);
+    echo "${key}は, ${price}円です。";
     echo '<br>';
-}
+    $i++;
+   }
+
 ?>
+

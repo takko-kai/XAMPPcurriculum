dd8dc0fb078aa0fc43b8dde13e3e6f0d84c51536
takko-kai <116253178+takko-kai@users.noreply.github.com>
2022-11-25 17:30:41 +0900

    1-2

---
+++ 1-2/main.js
@@ -0,0 +1,21 @@
+console.log("Hello World!!");
+console.log(65536);
+console.log(10 + 5);
+console.log(10 * 5);
+console.log("Hello World!!");
+console.log("Hello " + "World!!");
+// 割り算の余りの値を求めたい場合は「%」を使用します。
+console.log(10 % 2);
+/*
+上記の場合、10割る2で余り0
+そのため出力は「0」
+下記の場合、出力は「1」になります
+*/
+console.log(10 % 3);
+//チャレンジ
+console.log("10 + 8");
+console.log(10 + 8);
+console.log("20割る3の余りは"+20 % 3);
+
+window.alert("Hello World!!");
+
